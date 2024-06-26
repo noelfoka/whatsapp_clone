1542fcca56bef668c02deae25b9ff1a69662e9b9
noelfoka <nomgnenoel@gmail.com>
2024-06-26 22:38:16 +0100

    start implement common button

--- src/components/common/IconButton.tsx
+++ src/components/common/IconButton.tsx
@@ -3,11 +3,12 @@ import React from "react";
 interface IconButtonProps {
   icon: React.ReactNode;
   handleFunction: () => void;
+  isActive?: boolean;
 }
 
-const IconButton: React.FC<IconButtonProps> = ({icon, handleFunction}) => {
+const IconButton: React.FC<IconButtonProps> = ({icon, handleFunction, isActive}) => {
   return ( 
-    <button className="w-8 h-8 rounded-full" onClick={handleFunction}>
+    <button className={`w-8 h-8 rounded-full hover:bg-gray-800 ${isActive ? "bg-gray-800" : "bg-transparent"}`} onClick={handleFunction}>
       {icon}
     </button>
    );
